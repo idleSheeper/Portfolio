fb462e9a60402b03a0ed807c9bf229008d62c112
idleSheeper <owiecB@gmail.com>
2017-01-09 12:57:24 +0100

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,31 +1,26 @@
 $(document).ready(function(){
     
-    console.log($(window).height());    
-     if($(window).width() > 970) {//992
+    // Set standard settings
+     if($(window).width() > 992) {//992
         $('#about, #portfolio').css('height',$(window).height());
         $('.projects').css('height', 'calc(' + $('#portfolio').css('height') + ' - ' + $('#portfolio h1').css('height') +')');
         $('.projects').attr('width', $('.projects').css('width')); 
         $('.projects').addClass('overflow-scroll');
         $('.arrow').addClass('arrow-rotate');
-      //  $('.projects').hide();
        }
     else {
         $('#portfolio h1').css('box-shadow', '0px 0px 5px 10px black');
         $('.poster').addClass('font-for-sm');
     }
-
-
-    var rope = 0;
     
-    // Resize window -> change height
+    // Resize window -> change height of .Portfolio
     $( window ).resize(function() {
-       if($(window).width() > 970) {
+       if($(window).width() > 992) {
             $('#about, #portfolio').css('height',$(window).height());
             $('.projects').css('height', 'calc(' + $('#portfolio').css('height') + ' - ' + $('#portfolio h1').css('height') +')');
             $('.projects').addClass('overflow-scroll');
             $('.arrow').addClass('arrow-rotate');
             $('#portfolio h1').css('box-shadow', '5px 0px 5px 5px black');
-           $('.poster').removeClass('font-for-sm');
        }
        else {
             $('#about, #portfolio, .projects').css('height', 'auto');
@@ -33,48 +28,46 @@ $(document).ready(function(){
             $('.arrow').removeClass('arrow-rotate');
             $('.projects').show();
             $('#portfolio h1').css('box-shadow', '0px 0px 5px 10px black');
-           $('.poster').addClass('font-for-sm');
        }
     });
     
-    
-    $('#portfolio h1').on('click', function(){
-        
-        if($(window).width() > 970) $('.projects').slideToggle();
-       
+    // show/hide projects for medium and large devices
+    $('#portfolio h1').on('click', function(){        
+        if($(window).width() > 992) $('.projects').slideToggle();       
     });
     
-    
+    // show/hide full poster for medium and large devices
     $('.poster').on('click', function(){        
-        if($(window).width() > 970) {
+        if($(window).width() > 992) {
             $('#portfolio').toggle();
             $(this).parent().toggleClass('col-md-10 col-md-4 about-zoom');
+            $('.hidden-p-poster').toggleClass('hidden-lg hidden-md')
             $(this).toggleClass('zoom ');
             $('#about .arrow').toggleClass('visible-md visible-lg hidden-md hidden-lg');
-            
-            console.log('zoomi');
         }
     });
     
+    // show/hide a signpost with contact
     $('.arrow').on('click', function(event){
         event.preventDefault();
-        if($(window).width() > 970)
+        if($(window).width() > 992)
         if ($(this).hasClass("isDown") ) {
             $(this).stop().animate({bottom:"-=50px"}, 200);          
             $(this).removeClass("isDown");
-        } else {
+        } 
+        else {
             $(this).stop().animate({bottom:"+=50px"}, 200);   
             $(this).addClass("isDown");
             console.log('dziala');
         };
-        return false;})
-            .children().on('click', function (event) {
+        return false;
+    }).children().on('click', function (event) {
         event.stopPropagation();
         });
     
+    // the effect of pulling noose
     $('#noose').on('click', function(){
         $(this).animate({height: '+=15px'}).animate({height: '-=15px'});
-         if($(window).width() > 970) $('.projects').slideToggle();
-    });
-    
+         if($(window).width() > 992) $('.projects').slideToggle();
+    });    
 });
